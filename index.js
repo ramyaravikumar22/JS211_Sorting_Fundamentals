afea2cf111c6b32f89739c728c9c85e81b78f966
ramyaravikumar22 <ramya38br@gmail.com>
2023-02-11 22:20:41 -0600

    saving filter, map, reduce methods

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //const ages = [23, 22, 51, 17, 32]
 
+//.map()
 
 const map = (arr, callback) => {
     let array = []
@@ -11,5 +12,31 @@ const map = (arr, callback) => {
 }
 
 const array1 = [1, 4, 9, 16]
-console.log(map(array1, x => x * 2))
+//console.log(map(array1, x => x * 2))
 
+//.filter()
+
+const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present', 'massachussetts']
+const filter = (arr, callback) => {
+    let array = []
+    for(let i = 0; i < arr.length; i++) {
+        if(callback(arr[i]) == true) {
+            array.push(arr[i])
+        }
+    }
+    return array
+}
+//console.log(filter(words, word => word.length > 6))*/
+
+
+//.reduce()
+const reduce = (arr) => {
+    let startingPoint = 0
+    for(const value of arr) {
+        startingPoint += value;
+    }
+    return startingPoint;
+}
+
+const numbers = [1, 2, 3, 4, 5]
+//console.log(reduce(numbers))
